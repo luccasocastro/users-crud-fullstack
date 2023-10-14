115d2d9cdb86b5b720e8ab6dc1f1f88b48ef212c
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-13 23:25:12 -0300

    feat: creating a service logics

--- frontend/src/app/app.service.ts
+++ frontend/src/app/app.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from './models/user.model';
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  private url = "http://localhost:8080/"
+
+  constructor(private http: HttpClient) { }
+
+  // Add user
+  addUser(user: User){
+    return this.http.post<User>(`${this.url}create`, user)
+  }
+
+  // Get users
+  getUsers(): Observable<any[]>{
+    return this.http.get<any[]>(this.url+'users')
+  }
+
+  // Get user by id
+  getUserById(id: number): Observable<User>{
+    return this.http.get<User>(`${this.url}user/${id}`)
+  }
+
+  // Update user
+  updateUser(id?: number, user?: any): Observable<any>{
+    return this.http.put<any>(`${this.url}user/update/${id}`, user)
+  }
+
+  // Delete user
+  deleteUser(id: number): Observable<any>{
+    return this.http.delete<any>(`${this.url}users/delete/${id}`)
+  }
 }
